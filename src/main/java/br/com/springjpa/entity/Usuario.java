68ab5d0f956802011ba27c8fdc5bd02e2d643d0b
Marcelo Domingues <mdomingues53@hotmail.com>
2024-05-22 13:23:15 -0300

    Primeiro Commit

---
+++ src/main/java/br/com/springjpa/entity/Usuario.java
@@ -0,0 +1,49 @@
+package br.com.springjpa.entity;
+
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+
+@Entity
+public class Usuario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	private String nome;
+
+	private String email;
+	
+	@ManyToMany
+	private Set<Role> roles;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
