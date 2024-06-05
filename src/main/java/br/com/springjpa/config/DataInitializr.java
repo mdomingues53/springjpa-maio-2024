13edb295e4dd584271722cc4cb6dbc20a7248af1
mdomingues53 <mdomingues53@hotmail.com>
2024-06-05 19:20:56 -0300

    Alterando a consulta para 7

--- src/main/java/br/com/springjpa/config/DataInitializr.java
+++ src/main/java/br/com/springjpa/config/DataInitializr.java
@@ -29,7 +29,7 @@ public class DataInitializr implements ApplicationListener<ContextRefreshedEvent
 		 * 7 - Consultar Aproximada Por Nome
 		 * 8 - Consultar Por Email
 		 */
-		int opcao = 6;
+		int opcao = 7;
 		
 		switch(opcao) {			
 			case 1 : 
