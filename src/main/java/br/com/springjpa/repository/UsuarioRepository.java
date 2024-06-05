a7bee97a9b390a442fdf40ce798972b65bc46ce1
mdomingues53 <mdomingues53@hotmail.com>
2024-06-05 18:38:46 -0300

    Inclusao de funcionalidades

--- src/main/java/br/com/springjpa/repository/UsuarioRepository.java
+++ src/main/java/br/com/springjpa/repository/UsuarioRepository.java
@@ -1,9 +1,18 @@
 package br.com.springjpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import br.com.springjpa.entity.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 
+	Usuario findByNomeIgnoreCase(String nome);
+	
+	@Query("select u from Usuario u where u.nome like %?1%")
+	Usuario findByLikeNome(String nome);
+	
+	
+	Usuario findByEmail(String email);
+	
 }
